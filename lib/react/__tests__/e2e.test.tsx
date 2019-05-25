85072927ef6d598cc74fe76b574ca52f5b5de52d
Tom Chmelevskij <tomas.chmelevskij@ocyan.com>
2019-05-25 10:21:59 +0100

    fix(react): ignore tests

--- lib/react/__tests__/e2e.test.tsx
+++ lib/react/__tests__/e2e.test.tsx
@@ -4,10 +4,6 @@ import * as React from 'react';
 import '../../../setupTests';
 import { Frontier } from '../frontier';
 
-describe('ignore this test suite', () => {
-  it('succeed', () => expect(1 + 1).toEqual(2));
-});
-
 describe('<Frontier> usage with render props', () => {
   const schema = require('../../../fixtures/data/tests-jsonschema.json');
   const mutation = gql`
@@ -45,10 +41,7 @@ describe('<Frontier> usage with render props', () => {
     </Frontier>
   );
 
-  it.todo('should set-up the fields with proper values', () => {
-    // tslint:disable-next-line no-console no-any
-    console.log((wrapper.find('input[name="name"]').getElement() as any).node.value);
-  });
+  it.todo('should set-up the fields with proper values');
 
   it.todo('should set-up the display proper errors');
 
